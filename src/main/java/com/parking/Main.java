734c2b7008b16bab73393847a7416da5394d1237
kumars14 <suraj.kumar1@autodesk.com>
2026-02-15 08:41:44 +0530

    fix parkign lot runnig code

--- src/main/java/com/parking/Main.java
+++ src/main/java/com/parking/Main.java
@@ -6,29 +6,31 @@ public class Main {
     ParkingLotService parkingLot = ParkingLotService.getInstance();
 
     ParkingLevel level1 = new ParkingLevel();
-    level1.addParkingSpot(new ParkingSpot(SpotType.COMPACT, null));
-    level1.addParkingSpot(new ParkingSpot(SpotType.REGULAR, null));
-    level1.addParkingSpot(new ParkingSpot(SpotType.LARGE, null));
+    level1.addParkingSpot(new ParkingSpot("123", SpotType.COMPACT, null));
+    level1.addParkingSpot(new ParkingSpot("234", SpotType.REGULAR, null));
+    level1.addParkingSpot(new ParkingSpot("456", SpotType.LARGE, null));
     parkingLot.addLevels(level1);
 
     // Factory pattern: create vehicles by type
     Vehicle compactVehicle = VehicleFactory.createVehicle(VehicleType.COMPACT, "123456");
     Vehicle regularVehicle = VehicleFactory.createVehicle(VehicleType.REGULAR, "678901");
 
-    ParkingSpot found = parkingLot.findParkingSpot(compactVehicle);
-    System.out.println("Found spot for compact: " + (found != null));
+    Ticket ticket1 = parkingLot.parkVehicle(compactVehicle);
+    if (ticket1 != null) {
+      System.out.println("Parked. Ticket #" + ticket1.getTicketId());
+    }
 
-    Ticket ticket = parkingLot.parkVehicle(regularVehicle);
-    if (ticket != null) {
-      System.out.println("Parked. Ticket #" + ticket.getTicketId());
+    Ticket ticket2 = parkingLot.parkVehicle(regularVehicle);
+    if (ticket2 != null) {
+      System.out.println("Parked. Ticket #" + ticket2.getTicketId());
     }
 
     // Strategy pattern: choose payment method when exiting
     PaymentStrategy cashPayment = new CashPaymentStrategy();
 
-    if (ticket != null) {
-      parkingLot.removeVehicle(ticket, cashPayment);
-      System.out.println("Vehicle removed and payment done.");
+    if (ticket2 != null) {
+      parkingLot.removeVehicle(ticket2, cashPayment);
+      System.out.println("Vehicle removed and payment done. " + ticket2.getVehicle().getLicensePlate());
     }
   }
 }
